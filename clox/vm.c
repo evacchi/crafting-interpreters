9d8bd7f425099826d334c966e0d332d44fd3c949
Edoardo Vacchi <uncommonnonsense@gmail.com>
2020-10-10 13:36:56 +0200

    Chapter 21.2

--- clox/vm.c
+++ clox/vm.c
@@ -110,6 +110,7 @@ static InterpretResult run() {
       case OP_NIL:      push(NIL_VAL); break;
       case OP_TRUE:     push(BOOL_VAL(true)); break;
       case OP_FALSE:    push(BOOL_VAL(false)); break;
+      case OP_POP:      pop(); break;
       case OP_EQUAL: {
         Value a = pop();
         Value b = pop();
@@ -147,9 +148,15 @@ static InterpretResult run() {
 
         push(NUMBER_VAL(-AS_NUMBER(pop())));
         break;
-      case OP_RETURN: {
+      
+      case OP_PRINT: {
         printValue(pop());
         printf("\n");
+        break;
+      }
+
+      case OP_RETURN: {
+        // Exit interpreter.
         return INTERPRET_OK;
       }
     }
