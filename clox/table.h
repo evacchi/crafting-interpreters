36079fb6b63c7f860b7a4e2b02c5ba560b308cd7
Edoardo Vacchi <uncommonnonsense@gmail.com>
2020-10-19 21:02:11 +0200

    Chapter 26.4

--- clox/table.h
+++ clox/table.h
@@ -22,8 +22,9 @@ bool tableGet(Table* table, ObjString* key, Value* value);
 bool tableSet(Table* table, ObjString* key, Value value);
 bool tableDelete(Table* table, ObjString* key);
 void tableAddAll(Table* from, Table* to);
-void markTable(Table* table);
 ObjString* tableFindString(Table* table, const char* chars,
                            int length, uint32_t hash);
+void tableRemoveWhite(Table* table);
+void markTable(Table* table);
 
 #endif
