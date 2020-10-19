93c0cf66bb06349b5d05b017833e27d81bc9104d
Edoardo Vacchi <uncommonnonsense@gmail.com>
2020-10-19 19:05:10 +0200

    Chapter 26.2.0

--- clox/memory.c
+++ clox/memory.c
@@ -25,6 +25,22 @@ void* reallocate(void* pointer, size_t oldSize, size_t newSize) {
   return result;
 }
 
+void markObject(Obj* object) {
+  if (object == NULL) return;
+#ifdef DEBUG_LOG_GC
+  printf("%p mark ", (void*)object);
+  printValue(OBJ_VAL(object));
+  printf("\n");
+#endif
+
+  object->isMarked = true;
+}
+
+void markValue(Value value) {
+  if (!IS_OBJ(value)) return;
+  markObject(AS_OBJ(value));
+}
+
 static void freeObject(Obj* object) {
 #ifdef DEBUG_LOG_GC
   printf("%p free type %d\n", (void*)object, object->type);
@@ -64,10 +80,19 @@ static void freeObject(Obj* object) {
   }
 }
 
+static void markRoots() {
+  for (Value* slot = vm.stack; slot < vm.stackTop; slot++) {
+    markValue(*slot);
+  }
+  markTable(&vm.globals);
+}
+
 void collectGarbage() {
 #ifdef DEBUG_LOG_GC
   printf("-- gc begin\n");
 #endif
+
+  markRoots();
 
 #ifdef DEBUG_LOG_GC
   printf("-- gc end\n");
