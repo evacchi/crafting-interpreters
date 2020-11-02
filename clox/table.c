d849a28639690807209f340535002fa93e238d1f
Edoardo Vacchi <uncommonnonsense@gmail.com>
2020-11-02 16:57:11 +0100

    Chapter 30.2

--- clox/table.c
+++ clox/table.c
@@ -10,18 +10,18 @@
 
 void initTable(Table* table) {
   table->count = 0;
-  table->capacity = 0;
+  table->capacity = -1;
   table->entries = NULL;
 }
 
 void freeTable(Table* table) {
-  FREE_ARRAY(Entry, table->entries, table->capacity);
+  FREE_ARRAY(Entry, table->entries, table->capacity + 1);
   initTable(table);
 }
 
 static Entry* findEntry(Entry* entries, int capacity,
                         ObjString* key) {
-  uint32_t index = key->hash % capacity;
+  uint32_t index = key->hash & capacity;
   Entry* tombstone = NULL;
 
   for (;;) {
@@ -32,7 +32,7 @@ static Entry* findEntry(Entry* entries, int capacity,
         // Empty entry.
         return tombstone != NULL ? tombstone : entry;
       } else {
-        // We found a tombstone;
+        // We found a tombstone.
         if (tombstone == NULL) tombstone = entry;
       }
     } else if (entry->key == key) {
@@ -40,7 +40,7 @@ static Entry* findEntry(Entry* entries, int capacity,
       return entry;
     }
 
-    index = (index + 1) % capacity;
+    index = (index + 1) & capacity;
   }
 }
 
@@ -55,8 +55,8 @@ bool tableGet(Table* table, ObjString* key, Value* value) {
 }
 
 static void adjustCapacity(Table* table, int capacity) {
-  Entry* entries = ALLOCATE(Entry, capacity);
-  for (int i = 0; i < capacity; i++) {
+  Entry* entries = ALLOCATE(Entry, capacity + 1);
+  for (int i = 0; i <= capacity; i++) {
     entries[i].key = NULL;
     entries[i].value = NIL_VAL;
   }
@@ -64,7 +64,7 @@ static void adjustCapacity(Table* table, int capacity) {
   // recalculate the buckets for each of the existing entries
   // (rebuild the table from scratch by re-inserting)
   table->count = 0;
-  for (int i = 0; i < table->capacity; i++) {
+  for (int i = 0; i <= table->capacity; i++) {
     Entry* entry = &table->entries[i];
     if (entry->key == NULL) continue;
 
@@ -76,14 +76,14 @@ static void adjustCapacity(Table* table, int capacity) {
     table->count++;
   }
 
-  FREE_ARRAY(Entry, table->entries, table->capacity);
+  FREE_ARRAY(Entry, table->entries, table->capacity + 1);
   table->entries = entries;
   table->capacity = capacity;
 }
 
 bool tableSet(Table* table, ObjString* key, Value value) {
-  if (table->count + 1 > table->capacity * TABLE_MAX_LOAD) {
-    int capacity = GROW_CAPACITY(table->capacity);
+  if (table->count + 1 > (table->capacity + 1) * TABLE_MAX_LOAD) {
+    int capacity = GROW_CAPACITY(table->capacity + 1) - 1;
     adjustCapacity(table, capacity);
   }
 
@@ -112,7 +112,7 @@ bool tableDelete(Table* table, ObjString* key) {
 }
 
 void tableAddAll(Table* from, Table* to) {
-  for (int i = 0; i < from->capacity; i++) {
+  for (int i = 0; i <= from->capacity; i++) {
     Entry* entry = &from->entries[i];
     if (entry->key != NULL) {
       tableSet(to, entry->key, entry->value);
@@ -124,7 +124,7 @@ ObjString* tableFindString(Table* table, const char* chars,
                            int length, uint32_t hash) {
   if (table->count == 0) return NULL;
 
-  uint32_t index = hash % table->capacity;
+  uint32_t index = hash & table->capacity;
 
   for (;;) {
     Entry* entry = &table->entries[index];
@@ -139,12 +139,12 @@ ObjString* tableFindString(Table* table, const char* chars,
       return entry->key;
     }
 
-    index = (index + 1) % table->capacity;
+    index = (index + 1) & table->capacity;
   }
 }
 
 void tableRemoveWhite(Table* table) {
-  for (int i = 0; i < table->capacity; i++) {
+  for (int i = 0; i <= table->capacity; i++) {
     Entry* entry = &table->entries[i];
     if (entry->key != NULL && !entry->key->obj.isMarked) {
       tableDelete(table, entry->key);
@@ -154,7 +154,7 @@ void tableRemoveWhite(Table* table) {
 }
 
 void markTable(Table* table) {
-  for (int i = 0; i < table->capacity; i++) {
+  for (int i = 0; i <= table->capacity; i++) {
     Entry* entry = &table->entries[i];
     markObject((Obj*)entry->key);
     markValue(entry->value);
