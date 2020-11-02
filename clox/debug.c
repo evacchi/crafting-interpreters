b55202ffe471792ccf486c56cdaa6aeb48115b55
Edoardo Vacchi <uncommonnonsense@gmail.com>
2020-11-02 21:34:44 +0100

    Chapter 21 - fix print

--- clox/debug.c
+++ clox/debug.c
@@ -68,10 +68,10 @@ int disassembleInstruction(Chunk* chunk, int offset) {
       return simpleInstruction("OP_DIVIDE", offset);
     case OP_NOT:
       return simpleInstruction("OP_NOT", offset);
-    case OP_PRINT:
-      return simpleInstruction("OP_PRINT", offset);
     case OP_NEGATE:
       return simpleInstruction("OP_NEGATE", offset);
+    case OP_PRINT:
+      return simpleInstruction("OP_PRINT", offset);
     case OP_RETURN:
       return simpleInstruction("OP_RETURN", offset);
     default:
