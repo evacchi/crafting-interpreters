1f5e0aac7bb74f35590970f7efe9c2e1e9f709c4
Edoardo Vacchi <uncommonnonsense@gmail.com>
2020-11-02 21:32:21 +0100

    Chapter 27 - fix

--- clox/compiler.c
+++ clox/compiler.c
@@ -666,8 +666,8 @@ static void classDeclaration() {
   emitBytes(OP_CLASS, nameConstant);
   defineVariable(nameConstant);
 
-  consume(TOKEN_LEFT_BRACE, "Expect '{' before class body");
-  consume(TOKEN_RIGHT_BRACE, "Expect '}' after class body");
+  consume(TOKEN_LEFT_BRACE, "Expect '{' before class body.");
+  consume(TOKEN_RIGHT_BRACE, "Expect '}' after class body.");
 }
 
 static void funDeclaration() {
